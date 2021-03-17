7a68ba02f23135c1a9793c8612fe2a8ac0c64457
Oscar <o.ceretti@reply.it>
2021-03-17 19:10:25 +0100

    [Wireframe]
    // Creato progetto
    // aggiunti domain presentation e data layers
    // inizializzato pod

---
+++ CleanArch/CleanArch/CleanArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchApp.swift
+//  CleanArch
+//
+//  Created by Oscar Ceretti on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArch/CleanArch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanArch
+//
+//  Created by Oscar Ceretti on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
